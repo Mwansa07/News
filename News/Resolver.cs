f5f7e4ea22a020234adf85488a31ccea56a0c2a0
Mwansa Pembanyali <mwansap07@gmail.com>
2020-07-16 21:12:50 +0200

    feat: Added Resolver class for Autofac that creates objects based on the request type

---
+++ News/Resolver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Autofac;
+
+namespace News
+{
+    public static class Resolver
+    {
+        public static IContainer container;
+
+        public static void Initialize(IContainer container)
+        {
+            Resolver.container = container;
+        }
+
+        public static T Resolve<T>()
+        {
+            return container.Resolve<T>();
+        }
+    }
+}
