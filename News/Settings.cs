8c882ae434016dbc6f6f5e6e6e0452565c3cf214
Mwansa Pembanyali <mwansap07@gmail.com>
2020-07-20 01:52:36 +0200

    chore: Added newsapi key to project

---
+++ News/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace News
+{
+    public static class Settings
+    {
+        public static string NewsApiKey
+        {
+            get
+            {
+                return "17c4d6a88464440099b50c08cb1e275e";
+            }
+        }
+    }
+}
