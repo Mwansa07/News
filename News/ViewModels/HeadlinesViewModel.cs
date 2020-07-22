9e3daf453c13a6f2e962909d1b5ae6db636222f1
Mwansa Pembanyali <mwansap07@gmail.com>
2020-07-22 03:11:50 +0200

    feat: Added code to get news data using the NewsService class. Added code to store and encode the url from the item selected in the item list in the headlines view

--- News/ViewModels/HeadlinesViewModel.cs
+++ News/ViewModels/HeadlinesViewModel.cs
@@ -1,10 +1,46 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Input;
+using News.Models;
+using News.Services;
+using Xamarin.Forms;
+
 namespace News.ViewModels
 {
     public class HeadlinesViewModel : ViewModel
     {
-        public HeadlinesViewModel()
+        private readonly NewsService newsService;
+        public NewsResult CurrentNews { get; set; }
+
+        public HeadlinesViewModel(NewsService newsService)
         {
+            this.newsService = newsService;
         }
+
+        public async Task Initialize(string scope)
+        {
+            var resolvedScope = scope.ToLower() switch
+            {
+                "local" => NewsScope.Local,
+                "global" => NewsScope.Global,
+                "headlines" => NewsScope.Headlines,
+                _ => NewsScope.Headlines
+            };
+            await Initialize(resolvedScope);
+        }
+
+        private async Task Initialize(NewsScope scope)
+        {
+            CurrentNews = await newsService.GetNews(scope);
+        }
+
+        public ICommand ItemSelected =>
+            new Command(async (selectedItem) =>
+        {
+            var selectedArticle = selectedItem as Article;
+            var url = HttpUtility.UrlEncode(selectedArticle.Url);
+            // Placeholder for more code later on
+        });
     }
 }
